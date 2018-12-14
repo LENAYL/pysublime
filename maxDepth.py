b3e986c4727fde1d46ffe936e477308ec2999a07
LENAYL <yanlina94@163.com>
2018-12-14 19:40:05 +0800

    git test.

---
+++ maxDepth.py
@@ -0,0 +1,65 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if  not root:
+            return 0
+        depth = 0
+        global count1
+        global count2
+        count2 = 0
+        count1 = 0
+
+        def depthCount(x, count):
+            global count1
+            global count2
+            if x:
+                count += 1
+            if x.left:
+                count1 = depthCount(x.left, count)
+            if x.right:
+                count2 = depthCount(x.right, count)
+            return max(count, count1, count2)
+            # return count
+        return depthCount(root, depth)
+
+s =Solution()
+root = TreeNode(1)
+one_1 = TreeNode(2)
+one_2 = TreeNode(2)
+two_1 = TreeNode(3)
+two_2 = TreeNode(4)
+two_3 = TreeNode(4)
+two_4 = TreeNode(3)
+three_1 = TreeNode(5)
+three_2 = TreeNode(6)
+three_3 = TreeNode(7)
+three_4 = TreeNode(8)
+three_5 = TreeNode(8)
+three_6 = TreeNode(7)
+three_7 = TreeNode(6)
+three_8 = TreeNode(5)
+root.left = one_1
+root.right = one_2
+one_1.left = two_1
+one_1.right = two_2
+one_2.left = two_3
+one_2.right = two_4
+
+two_1.left = three_1
+two_1.right = three_2
+two_2.left = three_3
+two_2.right = three_4
+two_3.left = three_5
+two_3.right = three_6
+two_4.left = three_7
+two_4.right = three_8
+print(s.maxDepth(root))
