6dbfddbd40e1d6d17588846b6b2abb502d88c888
LENAYL <yanlina94@163.com>
2018-12-21 18:59:17 +0800

    git test.

---
+++ reverseBits_190.py
@@ -0,0 +1,41 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        # method2
+        res = 0
+        count = 0
+        while n:
+            res = res*2 +n%2
+            n = n//2
+            count += 1
+        while count < 32:
+            res = res*2
+            count += 1
+        return res
+
+
+
+
+
+
+
+        # method1 慢慢慢
+        # res = ''
+        # while n:
+        #     rem = n % 2
+        #     n = n//2
+        #     res = res +''.join(str(rem))
+        #     l = len(res)
+        # if l < 32:
+        #     for i in range(32-l):
+        #         res = res + ''.join('0')
+        #     # res[:] = res +[0]*(32 - l)
+        # print(len(res))
+        # print(res)
+        # return int(res, 2)
+
+s = Solution()
+n = 15
+print(s.reverseBits(n))
+
