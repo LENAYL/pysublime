3e78443b8cec7fef5af6c55d0b7520b0ec2a9387
LENAYL <yanlina94@163.com>
2018-12-26 21:19:22 +0800

    git test.

--- isIsomorphic_205.py
+++ isIsomorphic_205.py
@@ -1,20 +1,20 @@
 #  method 2
-class Solution(object):
-    def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        return self.isomorphic(s)==self.isomorphic(t)
-    def  isomorphic(self, t):
-        s=[]
-        d={}
-        for i in range(len(t)):
-            if t[i] not in d:
-                d[t[i]]=i
-            s.append(d[t[i]])
-        return s
+# class Solution(object):
+#     def isIsomorphic(self, s, t):
+#         """
+#         :type s: str
+#         :type t: str
+#         :rtype: bool
+#         """
+#         return self.isomorphic(s)==self.isomorphic(t)
+#     def  isomorphic(self, t):
+#         s=[]
+#         d={}
+#         for i in range(len(t)):
+#             if t[i] not in d:
+#                 d[t[i]]=i
+#             s.append(d[t[i]])
+#         return s
 
 
 #  method 1
@@ -39,32 +39,32 @@ class Solution(object):
 #                 table[x] = t[i]
 #
 #         return True
-# class Solution(object):
-#
-#     def isIsomorphic(self, s, t):
-#         """
-#         :type s: str
-#         :type t: str
-#         :rtype: bool
-#         """
-#
-#         def issamestr( ss):
-#             if len(ss) < 2:
-#                 return True
-#             else:
-#                 list = [1] * len(ss)
-#                 i = 0
-#                 for j in range(1, len(ss)):
-#                     if ss[j - 1] == ss[j]:
-#                         list[i] += 1
-#                     else:
-#                         i += 1
-#             return list
-#
-#         if len(s) == len(t):
-#             return issamestr(s) == issamestr(t)
-#         else:
-#             return False
+class Solution(object):
+
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        def issamestr(ss):
+            if len(ss) < 2:
+                return True
+            else:
+                list = [1] * len(ss)
+                i = 0
+                for j in range(1, len(ss)):
+                    if ss[j - 1] == ss[j]:
+                        list[i] += 1
+                    else:
+                        i += 1
+            return list
+
+        if len(s) == len(t):
+            return issamestr(s) == issamestr(t)
+        else:
+            return False
 a = Solution()
 s = 'happy'
 t = 'ahnnc'
