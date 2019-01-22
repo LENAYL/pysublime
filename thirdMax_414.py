eae0dcc81cf6c7bcd92d094af241c21a202e55a2
LENAYL <yanlina94@163.com>
2019-01-22 17:54:56 +0800

    git test.

--- thirdMax_414.py
+++ thirdMax_414.py
@@ -4,23 +4,32 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        # new_nums = list(set(sorted(nums)))
-        table = {}
-        for i in nums:
-            if i in table:
-                table[i] += 1
-            else:
-                table[i] = 1
-        new_nums = list(sorted(table))
-        return new_nums[-3] if len(new_nums) >= 3 else new_nums[-1]
+        #   method 1
+        # table = {}
+        # for i in nums:
+        #     if i in table:
+        #         table[i] += 1
+        #     else:
+        #         table[i] = 1
+        # new_nums = list(sorted(table))
+        # return new_nums[-3] if len(new_nums) >= 3 else new_nums[-1]
 
-        # if len(new_nums) < 3:
-        #     return new_nums[-1] if nums.count(new_nums[0]) > 1 else new_nums[0]
-        #
-        # return new_nums[-3]
+#          method 2
+        res = [-2**31-1, -2**31-1, -2**31-1]
+        for i in nums:
+            if i > res[0]:
+                res[0], res[1], res[2] = i, res[0], res[1]
+            elif i > res[1]:
+                res[1], res[2] = i, res[1]
+            elif i > res[2]:
+                res[2] = i
+        print(res)
+        return res[-1] if res[2] != -2**31-1 else res[0]
+
+
 
 
 
 s = Solution()
-nums = [1,1,2,2,2,3,3,3,4,4,4,5,6]
+nums =[2,1,3,2,1]
 print(s.thirdMax(nums))
